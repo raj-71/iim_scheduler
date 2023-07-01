98126ac5caf6eeb499e21d5937426d4a39280888
Raj Kumar <raj.kumar01@indiamart.com>
2023-07-01 23:49:13 +0530

    data update scheduler added

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const axios = require("axios");
-const express = require("express");
 const fs = require("fs");
-const csv = require("csvtojson");
 const jsonData = require("./data.json");
+const { JSDOM } = require("jsdom");
 const TelegramBot = require("node-telegram-bot-api");
 const Calendar = require("telegram-inline-calendar");
+const cron = require("node-cron");
 require("dotenv").config();
 
-const csvFile = "./data.csv";
 const jsonFile = "./data.json";
 
-const app = express();
-
 const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR2f0bJ4xN1z9QROs1QrC1UxMeEQ09uaw3WSzRWvbxlpLWUXI1Jh8aC42CMUjAcNrgtlFxBBMeIuB4G/pubhtml?gid=1865835579&single=true&urp=gmail_link&gxid=-8203366";
-const port = 5000;
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 const calendar = new Calendar(bot, {
@@ -21,6 +17,9 @@ const calendar = new Calendar(bot, {
     langugage: 'en'
 });
 
+
+
+///////////////////////////  FUNCTIONS  ///////////////////////////
 
 const courses = [
     {
@@ -130,70 +129,107 @@ const timeFormating = (time) => {
 
 
 const sheetRequest = async () => {
-    await axios.get(url).then((res) => {
-        let dataFromSheet = res.data;
+    try {
+        const res = await axios.get(url);
 
-        const newJSON = [];
+        if(res.data){
+            htmlToJSON(res.data);
+        }
 
-        csv()
-            .fromFile(csvFile)
-            .then((jsonObj) => {
-                let jsonString = JSON.stringify(jsonObj, null, 2);
-                let time = "";
-                jsonObj.forEach((item, index) => {
-                    // console.log(item);
-                    if (item.field4.length) {
-                        if (isNaN(item.field4.charAt(0)))
-                            time = item.field4;
-                        else
-                            time = timeFormating(item.field4);
-                    }
-                    if (item.field2.length) {
-                        let temp = {
-                            date: item.field2,
-                            day: item.field3,
-                            schedule: [],
-                        };
-                        newJSON.push(temp);
-
-                        let courseTemp = extractCourseInfo(item.field5);
-
-                        if (courseTemp) {
-                            courseTemp.time = time;
-                        }
-
-                        newJSON[newJSON.length - 1].schedule.push(courseTemp);
-                    } else {
-                        let temp = extractCourseInfo(item.field5);
-
-                        if (temp) {
-                            temp.time = time;
-                        }
-                        newJSON[newJSON.length - 1].schedule.push(Object(temp));
-                    }
-                });
-
-                jsonString = JSON.stringify(newJSON, null, 2);
-
-                fs.writeFile(jsonFile, jsonString, (err) => {
-                    if (err) {
-                        console.error("Error writing JSON file:", err);
-                    } else {
-                        console.log("CSV file converted to JSON successfully!");
-                    }
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    });
+    } catch (error) {
+        console.log("Error in getting sheet data: ", error);
+    }
 };
+
+
+const htmlToJSON = (htmlData) => {
+    const dom = new JSDOM(htmlData);
+
+    const table = dom.window.document.querySelector('table');
+
+    const rows = table.querySelectorAll("tr");
+    const data = [];
+
+    rows.forEach((row) => {
+        const rowData = {};
+        const cells = row.querySelectorAll("td");
+
+        cells.forEach((cell, index) => {
+            rowData[`field${index+1}`] = cell.textContent;
+        });
+
+        if(Object.keys(rowData).length !== 0)
+            data.push(rowData);
+    });
+
+    const newJSON = [];
+
+    data.forEach((item) => {
+        if(item.field3.length){
+            if(isNaN(item.field3.charAt(0)))
+                time = item.field3;
+            else
+                time = timeFormating(item.field3);
+        }
+        if(item.field1.length){
+            let temp = {
+                date: item.field1,
+                day: item.field2,
+                schedule: [],
+            };
+            newJSON.push(temp);
+
+            let courseTemp = extractCourseInfo(item.field4);
+
+            if(courseTemp){
+                courseTemp.time = time;
+            }
+
+            newJSON[newJSON.length - 1].schedule.push(courseTemp);
+        } else {
+            let temp = extractCourseInfo(item.field4);
+
+            if(temp){
+                temp.time = time;
+            }
+            newJSON[newJSON.length - 1].schedule.push(Object(temp));
+        }
+    });
+
+    let jsonData = JSON.stringify(newJSON, null, 2);
+
+    fs.writeFile(jsonFile, jsonData, (err) => {
+        if(err){
+            console.log("Error in writing JSON file: ", err);
+        } else {
+            console.log("JSON file written successfully");
+        }
+    });
+}
 
 
 const extractedCourses = (schedule) => schedule.filter(scheduleItem => {
     return courses.some(course => course.course === scheduleItem.courseName && course.section === scheduleItem.section);
 });
 
+
+///////////////////////////////////////////////////////////////////
+// UPDATE DATE AND TIME HERE
+///////////////////////////////////////////////////////////////////
+
+
+function updateData(){
+    console.log('Updating data...')
+    sheetRequest();
+}
+
+
+cron.schedule('0 * * * *', updateData);
+
+
+/////////////////////////////////////////////
+//              BOT COMMANDS               //
+/////////////////////////////////////////////
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
@@ -205,6 +241,7 @@ bot.onText(/\/start/, (msg) => {
     };
     bot.sendMessage(chatId, 'Choose an option:', options);
 });
+
 
 bot.onText(/Today|Tomorrow|Choose Date/, async (msg, match) => {
     const chatId = msg.chat.id;
@@ -228,10 +265,19 @@ bot.on("callback_query", async (query) => {
             console.log("Selected Date: ", res);
             let date = new Date(res);
             let sch = await schedule(date);
-            bot.sendMessage(query.message.chat.id, "Schedule on " + date.toLocaleDateString('en-GB', {day: 'numeric', month: 'short', year: '2-digit', timeZone: 'Asia/Kolkata'}).replace(/ /g, '/') + '\n' +sch, {parse_mode: 'HTML'});
+            if(sch.length == 0)
+                bot.sendMessage(query.message.chat.id, "Yayyyyy! No class on " + date.toLocaleDateString('en-GB', {day: 'numeric', month: 'short', year: '2-digit', timeZone: 'Asia/Kolkata'}).replace(/ /g, '/') + ' 🥳🥳🥳');
+            else
+                bot.sendMessage(query.message.chat.id, "Schedule on " + date.toLocaleDateString('en-GB', {day: 'numeric', month: 'short', year: '2-digit', timeZone: 'Asia/Kolkata'}).replace(/ /g, '/') + '\n' + sch, {parse_mode: 'HTML'});
         }
     }
 })
+
+
+/////////////////////////////////////////////
+//              EXTRACT SCHEDULE           //
+/////////////////////////////////////////////
+
 
 
 const todaySchedule = async () => {
@@ -247,12 +293,9 @@ const tomorrowSchedule = async () => {
 }
 
 const schedule = async (dateIncoming) => {
-    console.log("date: ", dateIncoming);
     let date = dateIncoming.toLocaleDateString('en-GB', {
         day: 'numeric', month: 'short', year: '2-digit', timeZone: 'Asia/Kolkata'
     }).replace(/ /g, '/');
-
-    console.log(date);
 
     let data = [];
     await Promise.all(
@@ -276,37 +319,3 @@ const schedule = async (dateIncoming) => {
 
     return text;
 }
-
-
-// app.get("/", async (req, res) => {
-
-
-//     try {
-//         await sheetRequest();
-//     } catch (err) {
-//         console.log("Can't fetch sheet data!!!\n", err);
-//     }
-
-//     let today = new Date();
-//     let tomorrow = new Date(today);
-//     tomorrow.setDate(today.getDate() + 1);
-
-//     let date = tomorrow.toLocaleDateString('en-GB', {
-//         day: 'numeric', month: 'short', year: '2-digit'
-//     }).replace(/ /g, '/');
-
-//     console.log(date);
-
-//     let data = [];
-//     await Promise.all(
-//         jsonData.map(async (item) => {
-//             // match course name
-//             if (item.date === date) {
-//                 data = extractedCourses(item.schedule);
-//             }
-//         })
-//     );
-//     res.send(data);
-// });
-
-// app.listen(port, () => console.log(`Server running on port ${port}`));
